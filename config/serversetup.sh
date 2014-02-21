d9d2abad7974937d7ae0884dc059ed6610ac2f9b
Mike De La Loza <mikedll@mikedll.com>
2014-02-21 15:50:41 -0800

    Nginx setup links that I'm using locally.

---
+++ config/serversetup.sh
@@ -0,0 +1,17 @@
+
+
+Follwing
+http://bclennox.com/extremely-large-file-uploads-with-nginx-passenger-rails-and-jquery
+for our first setup.
+
+Further reading:
+http://www.grid.net.ru/nginx/resumable_uploads.en.html
+
+
+Upload module:
+http://www.grid.net.ru/nginx/download/nginx_upload_module-2.2.0.zip
+
+Nginx, most recent compatible with upload module:
+http://nginx.org/download/nginx-1.2.9.tar.gz
+
+
