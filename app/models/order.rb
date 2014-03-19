ed9f8d9e95b69317a3ed0f1d5a3bbad7a62588cb
Arron Washington <arron@theradicaledwards.com>
2014-03-19 02:21:07 -0400

    Enforce email collection if we are preparing for payment.

--- app/models/order.rb
+++ app/models/order.rb
@@ -3,11 +3,13 @@ class Order < ActiveRecord::Base
     DRAFT = 'DRAFT'
   end
 
+  attr_accessor :preparing_for_payment
   has_many :order_files
 
   accepts_nested_attributes_for :order_files
   validates :secure_token, presence: true
   validates :status, presence: true
+  validates :cardholder_email, presence: true, if: :preparing_for_payment?
 
   before_validation :generate_secure_token
 
@@ -15,5 +17,9 @@ class Order < ActiveRecord::Base
 
   def generate_secure_token
     self.secure_token ||= SecureRandom.hex
+  end
+
+  def preparing_for_payment?
+    preparing_for_payment == true
   end
 end
--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -3,6 +3,14 @@ require 'spec_helper'
 describe Order do
   describe 'validations' do
     it { should validate_presence_of :status }
+    it 'should validate email presence if preparing_for_payment == true' do
+      order = build(:order, preparing_for_payment: true)
+      order.should validate_presence_of :cardholder_email
+    end
+    it 'should not validate email presence if preparing_for_payment != true' do
+      order = create(:order)
+      order.should_not validate_presence_of :cardholder_email
+    end
   end
 
   describe 'secure tokens' do
