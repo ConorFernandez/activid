4a894b4d4447102cbb0d3bc173890068f50952fa
Arron Washington <arron@theradicaledwards.com>
2014-04-09 19:41:32 -0400

    Enable SendGrid for production emails.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,4 +79,14 @@ Activid::Application.configure do
   config.log_formatter = ::Logger::Formatter.new
 
   config.action_mailer.default_url_options = { host: 'activid.co' }
+
+  ActionMailer::Base.smtp_settings = {
+      :address        => 'smtp.sendgrid.net',
+      :port           => '587',
+      :authentication => :plain,
+      :user_name      => ENV['SENDGRID_USERNAME'],
+      :password       => ENV['SENDGRID_PASSWORD'],
+      :domain         => 'heroku.com',
+      :enable_starttls_auto => true
+  }
 end
