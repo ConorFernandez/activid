3fbf00152f1a6fd327f7d769241948d236b68ee2
Mark Bertrand <marksbertrand@gmail.com>
2014-07-16 16:57:04 -0700

    VIdeo layout cleanup and adding all videos

--- app/assets/javascripts/videos.js
+++ app/assets/javascripts/videos.js
@@ -12,13 +12,12 @@ $(document).ready(function () {
         }, 750);
     });
     
-    $("#video").click(function() {
-       var src = $(".vid-src").text();
-       var title = $(".vid-title").text();
-       var desc =  $(".vid-desc").text();
-       $("#vid_frame").attr("src", src);
-       $("#vid-main-title").text(title);
-       $("#vid-main-description").text(desc);
-    })
-
+    $(".vid-item").click(function(e) {
+      var src = $(this).find(".vid-src").text();
+      var title = $(this).find(".vid-title").text();
+      var desc = $(this).find(".vid-desc").text();
+      $("#vid_frame").attr("src", src);
+      $("#vid-main-title").text(title);
+      $("#vid-main-description").text(desc);
+  })
 });
