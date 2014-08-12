d490342ea48110971ee56a011f564e6f78c96df6
Arron Washington <arron@theradicaledwards.com>
2014-08-12 18:20:30 -0400

    Force SSL.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  force_ssl if: :ssl_configured?
 
   protected
 
@@ -9,5 +10,9 @@ class ApplicationController < ActionController::Base
     secure_token = cookies[:order_secure_token]
     return nil if secure_token.blank?
     Order.where(secure_token: secure_token).first
+  end
+
+  def ssl_configured?
+    Rails.env.production?
   end
 end
